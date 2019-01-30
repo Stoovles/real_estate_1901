5dc5c0dc4cdeba79eff3ee74497a02e9ef581d49
Ethan Grab <ethangrab@gmail.com>
2019-01-30 10:24:46 -0700

    Iteration 3 complete

--- lib/house.rb
+++ lib/house.rb
@@ -13,4 +13,13 @@ attr_reader :price, :address, :room, :rooms
     @rooms << room
   end
 
+  def rooms_from_category(category_input)
+    rooms.find_all {|room| room.category == category_input}
+  end
+
+  def area
+    array_of_areas = rooms.map {|uniq_room| uniq_room.area(uniq_room.width, uniq_room.length)}
+    array_of_areas.sum
+  end
+
 end
