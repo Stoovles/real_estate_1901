38b908c746fd0e6b4d0de58678b60a16ace6c202
Ethan Grab <ethangrab@gmail.com>
2019-01-30 10:50:43 -0700

    Iteravtion 4 mostly complete - rooms_by_category and corresponding test to be completed

--- lib/house.rb
+++ lib/house.rb
@@ -22,4 +22,17 @@ attr_reader :price, :address, :room, :rooms
     array_of_areas.sum
   end
 
+  def price_per_square_foot
+    (@price / area.to_f).round(2)
+  end
+
+  def rooms_sorts_by_area
+    rooms.sort_by {|uniq_room| uniq_room.area(uniq_room.width, uniq_room.length)}
+  end
+
+  def rooms_by_category
+    #Out of gas
+    #find all unique categories and map to hash keys
+    #fill hash values with corresponding unique category using rooms_from_category(unique_category)
+  end
 end
--- test/house_test.rb
+++ test/house_test.rb
@@ -68,4 +68,45 @@ class HouseTest < Minitest::Test
     assert_equal 1900, house.area
   end
 
+  def test_returns_price_per_square_foot
+    house = House.new(400000, "123 sugar lane")
+    room_1 = Room.new(:bedroom, 10, 13)
+    room_2 = Room.new(:bedroom, 11, 15)
+    room_3 = Room.new(:living_room, 25, 15)
+    room_4 = Room.new(:basement, 30, 41)
+    house.add_room(room_1)
+    house.add_room(room_2)
+    house.add_room(room_3)
+    house.add_room(room_4)
+
+    assert_equal 210.53, house.price_per_square_foot
+  end
+
+  def test_returns_rooms_sorted_by_area
+    house = House.new(400000, "123 sugar lane")
+    room_1 = Room.new(:bedroom, 10, 13)
+    room_2 = Room.new(:bedroom, 11, 15)
+    room_3 = Room.new(:living_room, 25, 15)
+    room_4 = Room.new(:basement, 30, 41)
+    house.add_room(room_1)
+    house.add_room(room_2)
+    house.add_room(room_3)
+    house.add_room(room_4)
+
+    assert_equal [room_1, room_2, room_3, room_4], house.rooms_sorts_by_area
+  end
+
+  def test_returns_rooms_by_category
+    house = House.new(400000, "123 sugar lane")
+    room_1 = Room.new(:bedroom, 10, 13)
+    room_2 = Room.new(:bedroom, 11, 15)
+    room_3 = Room.new(:living_room, 25, 15)
+    room_4 = Room.new(:basement, 30, 41)
+    house.add_room(room_1)
+    house.add_room(room_2)
+    house.add_room(room_3)
+    house.add_room(room_4)
+
+    #assert_equal hash, house.rooms_by_category
+  end 
 end
